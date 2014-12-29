a2d44483f4492d8d1a71789af388d6eed40e94d6
amolv <amolvhankalas@gmail.com>
2014-12-29 16:02:00 +0530

    mobile folder delete

--- mobile/www/js/config.js
+++
@@ -1,8 +0,0 @@
-if (document.location.hostname == "localhost"){
-	var base = window.location.origin;
-    var baseURL = base +"/api/";
-    var logoURL = base +"/housingadmin2/logos/";
-} else {
-	var baseURL="http://node.fountaintechies.com:7000/api/";
-	var logoURL = "http://node.fountaintechies.com:7000/housingadmin2/logos/";
-} 
