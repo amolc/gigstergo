41026c52f900d99e8a634a827751e920650a9c66
lomteankush2 <lomteankush2@gmail.com>
2015-02-18 12:37:18 +0530

    mail changed

--- api/sendmail.js
+++ api/sendmail.js
@@ -2,21 +2,21 @@ var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
    auth: {
-       user: "ashishhsharma1990@gmail.com",
-       pass: "wastinglove"
+       user: "ankush.nek@gmail.com",
+       pass: "ankush2neknoor"
    }
 });
  exports.sendmail = function(req, res) {
 	smtpTransport.sendMail({
-   from: "ashishhsharma1990@gmail.com", // sender address
+   from: "ankush.nek@gmail.com", // sender address
    to: "ankush.lomte@fountaintechies.com", // comma separated list of receivers
-   subject: "Hello ✔", // Subject line
-   text: "Hello world this is my challange ✔" // plaintext body
+   subject: "Hello ..........", // Subject line
+   text: "Hello world this is my first mail" // plaintext body
 }, function(error, response){
    if(error){
        console.log(error);
    }else{
-       console.log("Message sent: " + response.message);
+       console.log("------------------------Message sent:  " + response.message);
    }
 });	
 };
