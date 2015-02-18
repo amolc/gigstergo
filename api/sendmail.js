821cb3e7addc8e0ca4cfedc712b8f8bc18f065c8
amolv <amolvhankalas@gmail.com>
2015-02-18 12:53:38 +0530

    sendmail

--- api/sendmail.js
+++ api/sendmail.js
@@ -1,15 +1,16 @@
 var nodemailer = require("nodemailer");
-var smtpTransport = nodemailer.createTransport("SMTP",{
-   service: "Gmail",
-   auth: {
-       user: "ankush.nek@gmail.com",
-       pass: "ankush2neknoor"
-   }
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'ankush.nek@gmail.com',
+        pass: 'ankush2neknoor'
+    }
 });
+
  exports.sendmail = function(req, res) {
-	smtpTransport.sendMail({
+	transporter.sendMail({
    from: "ankush.nek@gmail.com", // sender address
-   to: "ankush.lomte@fountaintechies.com", // comma separated list of receivers
+   to: "amol.vhankalas@fountaintechies.com", // comma separated list of receivers
    subject: "Hello ..........", // Subject line
    text: "Hello world this is my first mail" // plaintext body
 }, function(error, response){
