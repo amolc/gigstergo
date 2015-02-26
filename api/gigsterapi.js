de2fcef444cadbf459ed0de8c17e43ed2ed9f604
lomteankush2 <lomteankush2@gmail.com>
2015-02-26 18:45:41 +0530

    MC

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -60,9 +60,6 @@ exports.listgig = function(req, res) {
     var query = "SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city, tbl3.* FROM  btr_projects AS tbl1 LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT JOIN btr_users AS tbl3 ON tbl3.userId=tbl1.userId order by postedon DESC LIMIT 25";  
     //var query ="SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city, tbl3.profileimage, tbl4.bidfrom FROM  btr_projects AS tbl1 LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT JOIN btr_users AS tbl3 ON tbl3.userId=tbl1.userId LEFT JOIN btr_bids AS tbl4 ON tbl4.bidfrom=tbl3.userId order by postedon DESC LIMIT 25";
     /*var query =  "SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city , tbl3.profileimage,tbl4.* FROM btr_projects AS tbl1 LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT OUTER join btr_users AS tbl3 ON tbl3.userId=tbl1.userId LEFT join btr_bids AS tbl4 ON tbl4.bidfrom=tbl1.userId order by postedon DESC LIMIT 10";*/
-    //var query1="SELECT tbl3.profileimage FROM btr_projects AS tbl1 LEFT OUTER join btr_users AS tbl3 ON tbl3.userId=tbl1.userId order by postedon DESC LIMIT 10";
-   //var query = "SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city,tbl3.* FROM  btr_projects AS tbl1 LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT OUTER JOIN btr_bids AS tbl3 ON tbl3.bidfrom=22 and tbl3.projectId=tbl1.prjId order by postedon DESC";  
-//   var query = "SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city,tbl3.*,tbl4.profileimage,tbl4.username FROM btr_projects AS tbl1 LEFT OUTER join btr_users AS tbl4 ON tbl4.userId=tbl1.userId LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT OUTER JOIN btr_bids AS tbl3 ON tbl3.bidfrom=22 and tbl3.projectId=tbl1.prjId order by postedon DESC LIMIT 10";  
     console.log(query);
     db.query( query, function (err, val) {  
             
