638efeb0073fbffb36f4f2f186e191c623e1afa8
lomteankush2 <lomteankush2@gmail.com>
2015-02-26 14:03:54 +0530

    base

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -1,5 +1,5 @@
 
-	//var baseURL="http://node.fountaintechies.com:7000/api/";
-	var baseURL="http://localhost:7000/api/";
+	var baseURL="http://node.fountaintechies.com:7000/api/";
+//	var baseURL="http://localhost:7000/api/";
 	var logoURL = "http://node.fountaintechies.com:7000/housingadmin2/logos/";
  
