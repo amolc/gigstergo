9c7b1e4cf1b4a46e09089e7ceb06026bb4dba2b1
amolv <amolvhankalas@gmail.com>
2015-02-27 19:23:12 +0530

    mc

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -74,10 +74,8 @@ exports.listgig = function(req, res) {
     //var query="SELECT tbl1.*,tbl2.fname, tbl2.lname,tbl2.city, tbl3.* , tbl4.* FROM  btr_projects AS tbl1 LEFT OUTER join btr_userprofile AS tbl2 ON tbl2.userId=tbl1.userId LEFT JOIN btr_users AS tbl3 ON tbl3.userId=tbl1.userId LEFT JOIN btr_bids AS tbl4 ON tbl4.bidfrom=tbl1.userId where tbl1.status='0' OR tbl1.status='1' OR tbl1.status='2' order by postedon DESC LIMIT 25";
     console.log(query);
     db.query( query, function (err, val) {  
-            
-        res.jsonp(val);
-
-      });      
+      res.jsonp(val);
+    });
 };
 
 exports.app = function(req ,res){
