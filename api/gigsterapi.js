f4bf0c2f094a8b96f1c07f5c4abd72702da1948d
amolv <amolvhankalas@gmail.com>
2015-02-27 18:37:37 +0530

    min api change

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -152,38 +152,30 @@ exports.signup=function(req,res){
 
 
 exports.bidongig=function(req,res){
-  console.log("For bib on");
-  console.log(req.body);
+  console.log("---------------------------------------------For bib on--------------------------------------");
+  
       bidCRUD.create({'bidfrom': req.body.currentuser,'bidon':req.body.bidon, 'projectId':req.body.record.prjId,'bidcontent':req.body.proposal,'bidprice':req.body.price}, function (err, vals){
-           
-            console.log(err);
-            var mailmatter="<p>Hello "+req.body.record.fname+" "+req.body.record.lname+" </p><p>You have received a new bid on your Gig <a href='#'></a> from </p><p></p><p><a href='#'>Click here to see more details and award this Gig.</a></p>";
-                       
+        console.log(err);
+        var mailmatter ="<p>Hello "+req.body.record.fname+" "+req.body.record.lname+" </p><p>You have received a new bid on your Gig <a href='#'></a> from </p><p></p><p><a href='#'>Click here to see more details and award this Gig.</a></p>";
+        btrmsgCRUD.create({ 
+            'msgfrom':req.body.currentuser,
+            'msgto':req.body.record.userId,
+            'msgcontent':mailmatter,
+            'msgon':req.body.bidon,
+            'projectId':req.body.record.prjId,
+            'isread':'0',
+            'msgtype':'r'
+          }, function (err, vals){
 
-              btrmsgCRUD.create({'msgfrom':req.body.currentuser,'msgto':req.body.record.userId, 'msgcontent':mailmatter,'msgon':req.body.bidon,'projectId':req.body.record.prjId,'isread':'0','msgtype':'r'}, function (err, vals){
-
-              console.log("vals");
               console.log(vals);
-      
-              var resdata={
-               status: false,
-               message :'Ooops! Error Occured...sefksdck'
+            var resdata={
+               status: true,
+               message :'Message added'
                };
 
-
               res.jsonp(resdata);   
-
-
-
-              });
-
-
-
-
-             
-             });
-
-     
+          });
+    });
 
 };
 
