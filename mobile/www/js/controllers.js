4e66f7d35ce373ae34a353c7f00e572a1c6b681e
mandar <mandarjoshi@fountaintechies.com>
2015-02-28 17:46:04 +0530

    ALIGN CODE

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1925,79 +1925,41 @@ $scope.sendmsg=function(data){
   }   
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
-
-
-
 
 //inject the twitterService into the controller
 .controller('TwitterController', function($scope, $q, twitterService) {
 
     $scope.tweets; //array of tweets
     
-    twitterService.initialize();
-
-    //using the OAuth authorization result get the latest 20 tweets from twitter for the user
-    $scope.refreshTimeline = function() {
+        twitterService.initialize();
+        //using the OAuth authorization result get the latest 20 tweets from twitter for the user
+        $scope.refreshTimeline = function() {
         twitterService.getLatestTweets().then(function(data) {
-            $scope.tweets = data;
+        $scope.tweets = data;
         });
     }
 
-    //when the user clicks the connect twitter button, the popup authorization window opens
-    $scope.connectButton = function() {
-        twitterService.connectTwitter().then(function() {
-            if (twitterService.isReady()) {
-                //if the authorization is successful, hide the connect button and display the tweets
-                $('#connectButton').fadeOut(function(){
-                    $('#getTimelineButton, #signOut').fadeIn();
-                    $scope.refreshTimeline();
-                });
-            }
+          //when the user clicks the connect twitter button, the popup authorization window opens
+  $scope.connectButton = function() {
+    twitterService.connectTwitter().then(function() {
+          if (twitterService.isReady()) {
+          //if the authorization is successful, hide the connect button and display the tweets
+          $('#connectButton').fadeOut(function(){
+          $('#getTimelineButton, #signOut').fadeIn();
+          $scope.refreshTimeline();
         });
     }
+        });
+  }
 
     //sign out clears the OAuth cache, the user will have to reauthenticate when returning
     $scope.signOut = function() {
         twitterService.clearCache();
         $scope.tweets.length = 0;
-        $('#getTimelineButton, #signOut').fadeOut(function(){
+      $('#getTimelineButton, #signOut').fadeOut(function(){
             $('#connectButton').fadeIn();
-        });
+      });
     }
 
     //if the user is a returning user, hide the sign in button and display the tweets
