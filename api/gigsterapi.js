ff8df6f8478a9ed36375968f6565164cccd301fb
mandar <mandarjoshi@fountaintechies.com>
2015-03-02 20:55:52 +0530

    api

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -87,7 +87,7 @@ exports.listgig = function(req, res) {
         db.query(query1, function(err, val2){
           console.log("val22222222222");
           console.log(val2);
-          val[project].bidders = val2; 
+          val.bidders = val2; 
           
         });
     }
