4f420731e909d4c82b6e23b47d72b624a95b206a
mandar <mandarjoshi@fountaintechies.com>
2015-03-02 15:45:37 +0530

    Browsegig api updated

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -1,5 +1,5 @@
 
 	var baseURL="http://node.fountaintechies.com:7000/api/";
-//	var baseURL="http://localhost:7000/api/";
+	//var baseURL="http://localhost:7000/api/";
 	var profileUrl = "http://node.fountaintechies.com:7000/profile/";
  
