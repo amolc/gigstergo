9ad57ef101fd99b50a415a0a8da6ec2b4d914126
mandar <mandarjoshi@fountaintechies.com>
2015-03-02 13:11:10 +0530

    MC

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -80,6 +80,8 @@ exports.listgig = function(req, res) {
       for (var project in val) {
         console.log("project    --------------------");
         console.log(project);
+        console.log(val[project].prjId);
+        console.log(val[project].username);
       var query1 = "select * FROM btr_bids,btr_users WHERE btr_bids.projectid=project.prjId and btr_bids.bidfrom=btr_users.userId and btr_bids.bidfrom=btr_users.userId";
       console.log(query1);
         db.query(query1, function(err, val2){
