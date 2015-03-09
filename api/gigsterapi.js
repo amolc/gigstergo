7c081d436af68cfd5ee693d72e00b3dc6704484d
mandar <mandarjoshi@fountaintechies.com>
2015-03-09 18:47:03 +0530

    console log is removed

--- api/gigsterapi.js
+++ api/gigsterapi.js
@@ -205,14 +205,14 @@ exports.verifyacc=function(req,res){
 //for verify account api end here
 exports.bidongig=function(req,res){
   console.log("For bib on");
-  console.log(req.body);
+  //console.log(req.body);
   bidCRUD.create({'bidfrom': req.body.currentuser,'bidon':req.body.bidon, 'projectId':req.body.record.prjId,'bidcontent':req.body.proposal,'bidprice':req.body.price}, function (err, vals){
     console.log(err);
     //var mailmatter="<p>Hello "+req.body.record.fname+" "+req.body.record.lname+" </p><p>You have received a new bid on your Gig <a href='#'></a> from </p><p></p><p><a href='#'>Click here to see more details and award this Gig.</a></p>";
     // insert message for inbox
     //btrmsgCRUD.create({'msgfrom':req.body.currentuser,'msgto':req.body.record.userId, 'msgcontent':mailmatter,'msgon':req.body.bidon,'projectId':req.body.record.prjId,'isread':'0','msgtype':'r'}, function (err, vals){
-      console.log("vals");
-      console.log(vals);
+    //  console.log("vals");
+    //  console.log(vals);
       var resdata={
         status: false,
         message :'Ooops! Error Occured...sefksdck'
