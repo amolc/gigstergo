c7899ee4597f3e0415861ccafa51a05acbe08e4a
amolv <amolvhankalas@gmail.com>
2015-03-10 14:26:25 +0530

    mc

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -3,7 +3,9 @@
 	//var baseURL="http://localhost:7000/api/";
 	var profileUrl = "http://node.fountaintechies.com:7000/profile/";
  
-if( typeof device ){
+ 
+ //typeof device
+if(  document.location.hostname == '192.168.1.3' ){
       device = {
         uuid : 1,
         platform : 'web'
