0083d78357f95db9c641385cdca581a03bed0b92
amolv <amolvhankalas@gmail.com>
2015-03-10 16:26:36 +0530

    mc

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -5,11 +5,11 @@
  
  
  //typeof device
- /*alert( document.location.hostname );
+ alert( 'hostname = ' + document.location.hostname );
 if(  document.location.hostname == '192.168.1.3' || document.location.hostname == '192.168.0.101' ){
       device = {
         uuid : 1,
         platform : 'web'
     }
     window.localStorage.setItem("token_id", 0 );
-   }*/
+   }
