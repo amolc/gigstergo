d0d334be13ec30160ed624b0d91149ef190b0bf3
amolv <amolvhankalas@gmail.com>
2015-03-10 13:57:10 +0530

    version

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -3,10 +3,10 @@
 	//var baseURL="http://localhost:7000/api/";
 	var profileUrl = "http://node.fountaintechies.com:7000/profile/";
  
-/*if( typeof device ){
+if( typeof device ){
       device = {
         uuid : 1,
         platform : 'web'
     }
     window.localStorage.setItem("token_id", 0 );
-   }*/
+   }
