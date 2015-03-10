5ef94a26aa4331c179baf7dbf688fbedfd58d7eb
amolv <amolvhankalas@gmail.com>
2015-03-10 20:05:55 +0530

    pem file change name

--- api/pushnotification.js
+++ api/pushnotification.js
@@ -28,8 +28,8 @@ exports.notification = function(req, res) {
   var iosOptions = {
     gateway: 'gateway.sandbox.push.apple.com', // this URL is different for Apple's Production Servers and changes when you go to production
     errorCallback: callback,
-    cert: '/home/node/gigster/assets/cert.pem',                 
-    key:  '/home/node/gigster/assets/key.pem',                 
+    cert: '/home/node/gigster/assets/gigstergoCert.pem',                 
+    key:  '/home/node/gigster/assets/gigsterKey.pem',                 
     passphrase: 'ferrari1234',                 
     port: 2195,                       
     enhanced: true,                   
