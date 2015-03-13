7ddd1f464b56da6292f6ef6288d76c08d248f61b
amolv <amolvhankalas@gmail.com>
2015-03-13 19:35:55 +0530

    push anropdi test

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -642,44 +642,131 @@ $scope.sendfeedback=function(feedback){
 
 .controller('mainloginctrl', function($scope , $http , $state , $ionicModal ,$stateParams, $location , OpenFB , $cordovaPush){
 
-  // device ready 
-   ionic.Platform.ready(function(){
+  $scope.notifications = [];
+  ionic.Platform.ready(function(){
+    alert('asasa');
     $scope.register();
   });
   $scope.register = function () {
-        var config = null;
+      var config = null;
 
+      if (ionic.Platform.isAndroid()) {
+          config = {
+              "senderID": "19731243997" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
+          };
+      }
+      else if (ionic.Platform.isIOS()) {
+          config = {
+              "badge": "true",
+              "sound": "true",
+              "alert": "true"
+          }
+      }
+
+      $cordovaPush.register(config).then(function (result) {
+          console.log("Register success " + result);
+          alert("Register success " + result);
+          $cordovaToast.showShortCenter('Registered for push notifications');
+          $scope.registerDisabled=true;
+          // ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
+          if (ionic.Platform.isIOS()) {
+              $scope.regId = result;
+              console.log( 'Token id ='+$scope.regId );
+              alert( 'Token id ='+$scope.regId );
+              ///storeDeviceToken("ios");
+          }
+      }, function (err) {
+          console.log("Register error " + err)
+      });
+  }
+
+  // Notification Received
+    $scope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+        console.log(JSON.stringify([notification]));
         if (ionic.Platform.isAndroid()) {
-            config = {
-                "senderID": "19731243997" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
-            };
+            handleAndroid(notification);
         }
         else if (ionic.Platform.isIOS()) {
-            config = {
-                "badge": "true",
-                "sound": "true",
-                "alert": "true"
-            }
+            handleIOS(notification);
+            $scope.$apply(function () {
+                $scope.notifications.push(JSON.stringify(notification.alert));
+            })
         }
+    });
 
-        $cordovaPush.register(config).then(function (result) {
-            console.log("Register success " + result);
-            alert("Register success " + result);
-            $cordovaToast.showShortCenter('Registered for push notifications');
-            $scope.registerDisabled=true;
-            // ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
-            if (ionic.Platform.isIOS()) {
-                $scope.regId = result;
-                console.log( 'Token id ='+$scope.regId );
-                alert( 'Token id ='+$scope.regId );
-                ///storeDeviceToken("ios");
-            }
-        }, function (err) {
-            console.log("Register error " + err)
-        });
+    // Android Notification Received Handler
+    function handleAndroid(notification) {
+        // ** NOTE: ** You could add code for when app is in foreground or not, or coming from coldstart here too
+        //             via the console fields as shown.
+        console.log("In foreground " + notification.foreground  + " Coldstart " + notification.coldstart);
+        if (notification.event == "registered") {
+            $scope.regId = notification.regid;
+            storeDeviceToken("android");
+        }
+        else if (notification.event == "message") {
+            $cordovaDialogs.alert(notification.message, "Push Notification Received");
+            $scope.$apply(function () {
+                $scope.notifications.push(JSON.stringify(notification.message));
+            })
+        }
+        else if (notification.event == "error")
+            $cordovaDialogs.alert(notification.msg, "Push notification error event");
+        else $cordovaDialogs.alert(notification.event, "Push notification handler - Unprocessed Event");
     }
 
+    // IOS Notification Received Handler
+    function handleIOS(notification) {
+        // The app was already open but we'll still show the alert and sound the tone received this way. If you didn't check
+        // for foreground here it would make a sound twice, once when received in background and upon opening it from clicking
+        // the notification when this code runs (weird).
+        if (notification.foreground == "1") {
+            // Play custom audio if a sound specified.
+            if (notification.sound) {
+                var mediaSrc = $cordovaMedia.newMedia(notification.sound);
+                mediaSrc.promise.then($cordovaMedia.play(mediaSrc.media));
+            }
 
+            if (notification.body && notification.messageFrom) {
+                $cordovaDialogs.alert(notification.body, notification.messageFrom);
+            }
+            else $cordovaDialogs.alert(notification.alert, "Push Notification Received");
+
+            if (notification.badge) {
+                $cordovaPush.setBadgeNumber(notification.badge).then(function (result) {
+                    console.log("Set badge success " + result)
+                }, function (err) {
+                    console.log("Set badge error " + err)
+                });
+            }
+        }
+        // Otherwise it was received in the background and reopened from the push notification. Badge is automatically cleared
+        // in this case. You probably wouldn't be displaying anything at this point, this is here to show that you can process
+        // the data in this situation.
+        else {
+            if (notification.body && notification.messageFrom) {
+                $cordovaDialogs.alert(notification.body, "(RECEIVED WHEN APP IN BACKGROUND) " + notification.messageFrom);
+            }
+            else $cordovaDialogs.alert(notification.alert, "(RECEIVED WHEN APP IN BACKGROUND) Push Notification Received");
+        }
+    }
+
+    // Stores the device token in a db using node-pushserver (running locally in this case)
+    //
+    // type:  Platform type (ios, android etc)
+    function storeDeviceToken(type) {
+        // Create a random userid to store with it
+        var user = { user: 'user' + Math.floor((Math.random() * 10000000) + 1), type: type, token: $scope.regId };
+        console.log("Post token for registered device with data " + JSON.stringify(user));
+
+        $http.post('http://192.168.1.16:8000/subscribe', JSON.stringify(user))
+            .success(function (data, status) {
+                console.log("Token stored, device is successfully subscribed to receive push notifications.");
+            })
+            .error(function (data, status) {
+                console.log("Error storing device token." + data + " " + status)
+            }
+        );
+    }
 
   if( window.localStorage.getItem('islogin')=='true' ){
     $state.go('app.listgig')
